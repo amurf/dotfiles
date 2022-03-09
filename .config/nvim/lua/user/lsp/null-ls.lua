12176c63e28908d48af20be7544119db3d26bf6e
ash <ash@strategicdata.com.au>
2022-03-09 14:59:15 +1100

    update to eslint_d in null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -11,9 +11,13 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+    null_ls.builtins.diagnostics.eslint_d, -- eslint or eslint_d
+    null_ls.builtins.code_actions.eslint_d, -- eslint or eslint_d
+    null_ls.builtins.formatting.eslint_d -- prettier, eslint, eslint_d, or prettierd
+
+		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		-- formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.stylua,
     -- diagnostics.flake8
 	},
 })
