8c205b76d1c2c87d0430a307d9f92ca66671076f
ash <ash@strategicdata.com.au>
2022-06-01 10:57:17 +1000

    ftplugin timing doesn't start server as expected

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -11,9 +11,9 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-    null_ls.builtins.diagnostics.eslint_d, -- eslint or eslint_d
-    null_ls.builtins.code_actions.eslint_d, -- eslint or eslint_d
-    null_ls.builtins.formatting.eslint_d -- prettier, eslint, eslint_d, or prettierd
+    -- null_ls.builtins.diagnostics.eslint_d, -- eslint or eslint_d
+    -- null_ls.builtins.code_actions.eslint_d, -- eslint or eslint_d
+    -- null_ls.builtins.formatting.eslint_d -- prettier, eslint, eslint_d, or prettierd
 
 		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		-- formatting.black.with({ extra_args = { "--fast" } }),
--- .config/nvim/lua/user/lsp/vue-inspect-package-json.lua
+++ .config/nvim/lua/user/lsp/vue-inspect-package-json.lua
@@ -11,26 +11,27 @@ function read_package_json()
   return jsonString
 end
 
-local package_json_string = read_package_json()
+  local package_json_string = read_package_json()
 
-if package_json_string ~= nil then
-  local package_json_obj = vim.fn.json_decode(package_json_string)
-  local dependencies = package_json_obj.dependencies
+  if package_json_string ~= nil then
+    local package_json_obj = vim.fn.json_decode(package_json_string)
+    local dependencies = package_json_obj.dependencies
 
-  local vue_or_nuxt_lib = dependencies.vue or dependencies.nuxt
+    local vue_or_nuxt_lib = dependencies.vue or dependencies.nuxt
 
-  if vue_or_nuxt_lib ~= nil then
-    local vue_version = vue_or_nuxt_lib:gsub('[%^%~]', '')
-    local major, minor, patch = string.match(vue_version, "(%d+)%.(%d+)%.(%d+)")
+    if vue_or_nuxt_lib ~= nil then
+      local vue_version = vue_or_nuxt_lib:gsub('[%^%~]', '')
+      local major, minor, patch = string.match(vue_version, "(%d+)%.(%d+)%.(%d+)")
 
-    if tonumber(major) == 2 then
-      require'lspconfig'.vuels.setup{}
-    else
-      require'lspconfig'.volar.setup{
-        on_attach = function(client, buffer) 
-          client.resolved_capabilities.document_formatting = false
-        end
-      }
+      if tonumber(major) == 2 then
+        require'lspconfig'.vuels.setup{}
+      else
+        require'lspconfig'.volar.setup{
+          filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+          on_attach = function(client, buffer)
+            client.resolved_capabilities.document_formatting = false
+          end
+        }
+      end
     end
   end
-end
