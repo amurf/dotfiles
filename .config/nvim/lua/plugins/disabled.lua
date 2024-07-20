c4c4c962f0252d78c34473a0b4aae278d41e0a41
ash <irashp@gmail.com>
2024-07-20 22:44:48 +1000

    add lazy vim

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "garymjr/nvim-snippets", disabled = false },
+  { "rafamadriz/friendly-snippets", disabled = false },
+}
