6be02ceea05fe9f029a377c5072164be956ea002
amurf <ash.d.murphy@gmail.com>
2024-07-21 17:11:14 +1000

    fix typo in disabled.lua

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "garymjr/nvim-snippets", disabled = false },
-  { "rafamadriz/friendly-snippets", disabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
