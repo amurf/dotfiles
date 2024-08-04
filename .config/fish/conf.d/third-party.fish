45f17b7e8724efc659afde07f006ac2e59779c0b
amurf <ash.d.murphy@gmail.com>
2024-08-04 15:55:49 +1000

    add orbstack to third party config

--- .config/fish/conf.d/third-party.fish
+++ .config/fish/conf.d/third-party.fish
@@ -1,2 +1,8 @@
 fzf --fish | source
 starship init fish | source
+source ~/.orbstack/shell/init.fish
+
+set orbstack_init ~/orbstack/shell/init.fish
+
+test -f $orbstack_init; and source $orbstack_init
+
