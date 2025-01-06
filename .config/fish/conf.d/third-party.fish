45bd8a2b4870f9cb8522b7a737da36949b8191b1
amurf <ash.d.murphy@gmail.com>
2025-01-07 09:44:21 +1100

    remove duplicate orbstack init

--- .config/fish/conf.d/third-party.fish
+++ .config/fish/conf.d/third-party.fish
@@ -1,8 +1,6 @@
 fzf --fish | source
 starship init fish | source
-source ~/.orbstack/shell/init.fish
 
 set orbstack_init ~/orbstack/shell/init.fish
-
 test -f $orbstack_init; and source $orbstack_init
 
