61228138c34143faf4d408a10796f237650ecb28
Ashley Murphy <ashley.murphy@okendo.io>
2026-04-10 09:14:35 +1000

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,8 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 fish_add_path $HOME/.antigravity/antigravity/bin
 fish_add_path $HOME/.local/bin
+
+set -gx PNPM_HOME "/Users/ash/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
